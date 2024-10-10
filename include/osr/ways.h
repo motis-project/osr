8c4d75afb754c0ea8b7892b93442a83db2f8bf4e
Felix Gündling <felix.guendling@gmail.com>
2024-10-10 13:17:22 +0200

    use cista simple io, move code to cc file

--- include/osr/ways.h
+++ include/osr/ways.h
@@ -137,139 +137,10 @@ struct node_properties {
 static_assert(sizeof(node_properties) == 3);
 
 struct ways {
-  ways(std::filesystem::path p, cista::mmap::protection const mode)
-      : p_{std::move(p)},
-        mode_{mode},
-        r_{mode == cista::mmap::protection::READ
-               ? routing::read(p_ / "routing.bin")
-               : cista::wrapped<routing>{cista::raw::make_unique<routing>()}},
-        node_to_osm_{mm("node_to_osm.bin")},
-        way_osm_idx_{mm("way_osm_idx.bin")},
-        way_polylines_{mm_vec<point>{mm("way_polylines_data.bin")},
-                       mm_vec<std::uint64_t>{mm("way_polylines_index.bin")}},
-        way_osm_nodes_{mm_vec<osm_node_idx_t>{mm("way_osm_nodes_data.bin")},
-                       mm_vec<std::uint64_t>{mm("way_osm_nodes_index.bin")}} {}
+  ways(std::filesystem::path, cista::mmap::protection);
 
-  void add_restriction(std::vector<resolved_restriction>& rs) {
-    using it_t = std::vector<resolved_restriction>::iterator;
-    utl::sort(rs, [](auto&& a, auto&& b) { return a.via_ < b.via_; });
-    utl::equal_ranges_linear(
-        begin(rs), end(rs), [](auto&& a, auto&& b) { return a.via_ == b.via_; },
-        [&](it_t const& lb, it_t const& ub) {
-          auto const range = std::span{lb, ub};
-          r_->node_restrictions_.resize(to_idx(range.front().via_) + 1U);
-          r_->node_is_restricted_.set(range.front().via_, true);
-
-          for (auto const& x : range) {
-            if (x.type_ == resolved_restriction::type::kNo) {
-              r_->node_restrictions_[x.via_].push_back(
-                  restriction{r_->get_way_pos(x.via_, x.from_),
-                              r_->get_way_pos(x.via_, x.to_)});
-            } else /* kOnly */ {
-              for (auto const [i, from] :
-                   utl::enumerate(r_->node_ways_[x.via_])) {
-                for (auto const [j, to] :
-                     utl::enumerate(r_->node_ways_[x.via_])) {
-                  if (x.from_ == from && x.to_ != to) {
-                    r_->node_restrictions_[x.via_].push_back(restriction{
-                        static_cast<way_pos_t>(i), static_cast<way_pos_t>(j)});
-                  }
-                }
-              }
-            }
-          }
-        });
-    r_->node_restrictions_.resize(node_to_osm_.size());
-  }
-
-  void connect_ways() {
-    auto pt = utl::get_active_progress_tracker_or_activate("osr");
-
-    {  // Assign graph node ids to every node with >1 way.
-      pt->status("Create graph nodes")
-          .in_high(node_way_counter_.size())
-          .out_bounds(50, 60);
-
-      auto node_idx = node_idx_t{0U};
-      node_way_counter_.multi_.for_each_set_bit([&](std::uint64_t const b_idx) {
-        auto const i = osm_node_idx_t{b_idx};
-        node_to_osm_.push_back(i);
-        ++node_idx;
-        pt->update(b_idx);
-      });
-      r_->node_is_restricted_.resize(to_idx(node_idx));
-    }
-
-    // Build edges.
-    {
-      pt->status("Connect ways")
-          .in_high(way_osm_nodes_.size())
-          .out_bounds(60, 90);
-      auto node_ways = mm_paged_vecvec<node_idx_t, way_idx_t>{
-          cista::paged<mm_vec32<way_idx_t>>{
-              mm_vec32<way_idx_t>{mm("tmp_node_ways_data.bin")}},
-          mm_vec<cista::page<std::uint32_t, std::uint16_t>>{
-              mm("tmp_node_ways_index.bin")}};
-      auto node_in_way_idx = mm_paged_vecvec<node_idx_t, std::uint16_t>{
-          cista::paged<mm_vec32<std::uint16_t>>{
-              mm_vec32<std::uint16_t>{mm("tmp_node_in_way_idx_data.bin")}},
-          mm_vec<cista::page<std::uint32_t, std::uint16_t>>{
-              mm("tmp_node_in_way_idx_index.bin")}};
-      node_ways.resize(node_to_osm_.size());
-      node_in_way_idx.resize(node_to_osm_.size());
-      for (auto const [osm_way_idx, osm_nodes, polyline] :
-           utl::zip(way_osm_idx_, way_osm_nodes_, way_polylines_)) {
-        auto pred_pos = std::make_optional<point>();
-        auto from = node_idx_t::invalid();
-        auto distance = 0.0;
-        auto i = std::uint16_t{0U};
-        auto way_idx = way_idx_t{r_->way_nodes_.size()};
-        auto dists = r_->way_node_dist_.add_back_sized(0U);
-        auto nodes = r_->way_nodes_.add_back_sized(0U);
-        for (auto const [osm_node_idx, pos] : utl::zip(osm_nodes, polyline)) {
-          if (pred_pos.has_value()) {
-            distance += geo::distance(pos, *pred_pos);
-          }
-
-          if (node_way_counter_.is_multi(to_idx(osm_node_idx))) {
-            auto const to = get_node_idx(osm_node_idx);
-            node_ways[to].push_back(way_idx);
-            node_in_way_idx[to].push_back(i);
-            nodes.push_back(to);
-
-            if (from != node_idx_t::invalid()) {
-              dists.push_back(static_cast<std::uint16_t>(std::round(distance)));
-            }
-
-            distance = 0.0;
-            from = to;
-
-            if (i == std::numeric_limits<std::uint16_t>::max()) {
-              fmt::println("error: way with {} nodes", osm_way_idx);
-            }
-
-            ++i;
-          }
-
-          pred_pos = pos;
-        }
-        pt->increment();
-      }
-
-      for (auto const x : node_ways) {
-        r_->node_ways_.emplace_back(x);
-      }
-      for (auto const x : node_in_way_idx) {
-        r_->node_in_way_idx_.emplace_back(x);
-      }
-    }
-
-    auto e = std::error_code{};
-    std::filesystem::remove(p_ / "tmp_node_ways_data.bin", e);
-    std::filesystem::remove(p_ / "tmp_node_ways_index.bin", e);
-    std::filesystem::remove(p_ / "tmp_node_in_way_idx_data.bin", e);
-    std::filesystem::remove(p_ / "tmp_node_in_way_idx_index.bin", e);
-  }
+  void add_restriction(std::vector<resolved_restriction>&);
+  void connect_ways();
 
   std::optional<way_idx_t> find_way(osm_way_idx_t const i) {
     auto const it = std::lower_bound(begin(way_osm_idx_), end(way_osm_idx_), i);
@@ -359,18 +230,8 @@ struct ways {
       return way_nodes_[w].back() == way_nodes_[w].front();
     }
 
-    static cista::wrapped<routing> read(std::filesystem::path const& p) {
-      auto mem = cista::file{p.generic_string().c_str(), "r"}.content();
-      auto const ptr = cista::deserialize<routing, kMode>(mem);
-      return {std::move(mem), ptr};
-    }
-
-    void write(std::filesystem::path const& p) {
-      auto mmap = cista::mmap{(p / "routing.bin").string().c_str(),
-                              cista::mmap::protection::WRITE};
-      auto writer = cista::buf<cista::mmap>(std::move(mmap));
-      cista::serialize<kMode>(writer, *this);
-    }
+    static cista::wrapped<routing> read(std::filesystem::path const&);
+    void write(std::filesystem::path const&);
 
     vec_map<node_idx_t, node_properties> node_properties_;
     vec_map<way_idx_t, way_properties> way_properties_;
