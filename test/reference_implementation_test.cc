a3e953f45fcce1c6ffc79e7ff829e646f39fc026
mority <mority@posteo.net>
2025-06-17 12:00:48 +0200

    rm speedup printing due to inconsistent results

--- test/reference_implementation_test.cc
+++ test/reference_implementation_test.cc
@@ -21,6 +21,7 @@
 #include "osr/routing/route.h"
 #include "osr/types.h"
 #include "osr/ways.h"
+#include "utl/parallel_for.h"
 
 namespace fs = std::filesystem;
 using namespace osr;
@@ -39,54 +40,64 @@ void run(ways const& w,
          lookup const& l,
          unsigned const n_samples,
          unsigned const max_cost) {
-  auto prng = std::mt19937{};
-  auto distr = std::uniform_int_distribution<std::uint32_t>{0, w.n_nodes() - 1};
-  auto n_congruent = 0U;
-  auto n_empty_matches = 0U;
+
+  auto const from_tos = [&]() {
+    auto prng = std::mt19937{};
+    auto distr =
+        std::uniform_int_distribution<std::uint32_t>{0, w.n_nodes() - 1};
+    auto from_tos = std::vector<std::pair<node_idx_t, node_idx_t>>{};
+    for (auto i = 0U; i != n_samples; ++i) {
+      from_tos.emplace_back(distr(prng), distr(prng));
+    }
+    return from_tos;
+  }();
+
+  auto n_congruent = std::atomic<unsigned>{0U};
+  auto n_empty_matches = std::atomic<unsigned>{0U};
   auto reference_times = std::vector<std::chrono::steady_clock::duration>{};
   auto experiment_times = std::vector<std::chrono::steady_clock::duration>{};
 
-  auto const node_pinned_matches = [&](location const& loc,
-                                       node_idx_t const n) {
-    auto matches = l.match<car>(loc, false, direction::kForward,
-                                kMaxMatchDistance, nullptr);
-    std::erase_if(matches, [&](auto const& wc) {
-      return wc.left_.node_ != n && wc.right_.node_ != n;
-    });
-    return matches;
-  };
-
-  for (auto i = 0U; i != n_samples; ++i) {
-    auto const from_node = node_idx_t{distr(prng)};
+  auto m = std::mutex{};
+  utl::parallel_for(from_tos, [&](std::pair<node_idx_t, node_idx_t> const
+                                      from_to) {
+    auto const from_node = from_to.first;
     auto const from_loc = location{w.get_node_pos(from_node)};
-    auto const from_matches = node_pinned_matches(from_loc, from_node);
-    auto const from_matches_span =
-        std::span{begin(from_matches), end(from_matches)};
-
-    auto const to_node = node_idx_t{distr(prng)};
+    auto const to_node = from_to.second;
     auto const to_loc = location{w.get_node_pos(to_node)};
-    auto const to_matches = node_pinned_matches(to_loc, to_node);
-    auto const to_matches_span = std::span{begin(to_matches), end(to_matches)};
 
+    auto const node_pinned_matches = [&](location const& loc,
+                                         node_idx_t const n) {
+      auto matches = l.match<car>(loc, false, direction::kForward,
+                                  kMaxMatchDistance, nullptr);
+      std::erase_if(matches, [&](auto const& wc) {
+        return wc.left_.node_ != n && wc.right_.node_ != n;
+      });
+      return matches;
+    };
+    auto const from_matches = node_pinned_matches(from_loc, from_node);
+    auto const to_matches = node_pinned_matches(to_loc, to_node);
     if (from_matches.empty() || to_matches.empty()) {
       ++n_empty_matches;
     }
+    auto const from_matches_span =
+        std::span{begin(from_matches), end(from_matches)};
+    auto const to_matches_span = std::span{begin(to_matches), end(to_matches)};
 
     auto const reference_start = std::chrono::steady_clock::now();
     auto const reference_path =
         route(w, l, search_profile::kCar, from_loc, to_loc, from_matches_span,
               to_matches_span, max_cost, direction::kForward, nullptr, nullptr,
               nullptr);
-    reference_times.emplace_back(std::chrono::steady_clock::now() -
-                                 reference_start);
+    auto const reference_time =
+        std::chrono::steady_clock::now() - reference_start;
 
     auto const experiment_start = std::chrono::steady_clock::now();
     auto const experiment_path =
         route(w, l, search_profile::kCar, from_loc, to_loc, from_matches_span,
               to_matches_span, max_cost, direction::kForward, nullptr, nullptr,
               nullptr);
-    experiment_times.emplace_back(std::chrono::steady_clock::now() -
-                                  experiment_start);
+    auto const experiment_time =
+        std::chrono::steady_clock::now() - experiment_start;
 
     EXPECT_EQ(reference_path.has_value(), experiment_path.has_value());
     if (reference_path && experiment_path) {
@@ -114,12 +125,16 @@ void run(ways const& w,
                 1000);
       };
 
-      print_result("reference_path", reference_path, reference_times.back());
-      print_result("experiment_path", experiment_path, experiment_times.back());
+      print_result("reference_path", reference_path, reference_time);
+      print_result("experiment_path", experiment_path, experiment_time);
     } else {
       ++n_congruent;
     }
-  }
+
+    auto const guard = std::lock_guard{m};
+    reference_times.emplace_back(reference_time);
+    experiment_times.emplace_back(experiment_time);
+  });
 
   auto const non_empty_congruent = n_congruent - n_empty_matches;
   auto const non_empty_samples = n_samples - n_empty_matches;
@@ -129,15 +144,6 @@ void run(ways const& w,
                (static_cast<double>(non_empty_congruent) /
                 static_cast<double>(non_empty_samples)) *
                    100);
-  if (n_congruent == n_samples) {
-    fmt::println(
-        "speedup: {:.2f}",
-        static_cast<double>(
-            std::reduce(begin(reference_times), end(reference_times)).count()) /
-            static_cast<double>(
-                std::reduce(begin(experiment_times), end(experiment_times))
-                    .count()));
-  }
 }
 
 TEST(reference_implementation, monaco) {
@@ -161,7 +167,7 @@ TEST(reference_implementation, hamburg) {
     GTEST_SKIP() << kDataDir << " does not exist";
   }
 
-  constexpr auto const kNumSamples = 1000U;
+  constexpr auto const kNumSamples = 5000U;
   constexpr auto const kMaxCost = 2 * 3600U;
 
   auto const w = osr::ways{kDataDir, cista::mmap::protection::READ};
@@ -176,7 +182,7 @@ TEST(reference_implementation, switzerland) {
     GTEST_SKIP() << kDataDir << " does not exist";
   }
 
-  constexpr auto const kNumSamples = 100U;
+  constexpr auto const kNumSamples = 1000U;
   constexpr auto const kMaxCost = 5 * 3600U;
 
   auto const w = osr::ways{kDataDir, cista::mmap::protection::READ};
