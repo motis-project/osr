8d9ae2cedd2a7ff5167afafc61adb8ce0073058b
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2025-09-04 09:43:05 +0200

    Simplify code to check profile parameters variant

--- test/routing/profile.cc
+++ test/routing/profile.cc
@@ -1,26 +1,10 @@
 #include "osr/routing/profile.h"
 #include "osr/routing/parameters.h"
 
-constexpr auto const kProfileCount =
-    std::variant_size_v<osr::profile_parameters>;
-
-template <std::size_t N>
-constexpr bool is_profile() {
-  return osr::IsProfileParameters<
-      std::variant_alternative_t<N, osr::profile_parameters>>;
+template <typename... Ts>
+constexpr bool is_parameters_variant(std::variant<Ts...> const&) {
+  static_assert((osr::IsProfileParameters<Ts> && ...));
+  return (osr::IsProfileParameters<Ts> && ...);
 }
 
-template <std::size_t N>
-constexpr bool is_profile_helper(bool valid) {
-  if constexpr (N == 0U) {
-    return valid && is_profile<N>();
-  } else {
-    return is_profile_helper<N - 1>(valid && is_profile<N>());
-  }
-}
-
-constexpr auto const is_parameters_variant =
-    is_profile_helper<kProfileCount - 1>(true);
-
-static_assert(kProfileCount > 0U);
-static_assert(is_parameters_variant);
+static_assert(is_parameters_variant(osr::profile_parameters{}));
