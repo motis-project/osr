6aeff59715573cad28b78dbb7f97a5ae8f13d56f
jendrikw <buhtig@jendrik.eu>
2026-03-27 16:25:29 +0100

    add unit tests to way costs for foot profile

---
+++ test/way_cost.cc
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+#include "osr/routing/profiles/foot.h"
+
+osr::cost_t cost(const osr::way_properties p) {
+  return osr::foot<false>::way_cost({}, p, osr::direction::kForward, 1);
+}
+
+TEST(way_cost_foot, combinations) {
+  EXPECT_EQ(osr::kInfeasible, cost({.is_foot_accessible_ = false, .is_bike_accessible_ = false, .is_sidewalk_separate_ = false,}));
+  EXPECT_EQ(osr::kInfeasible, cost({.is_foot_accessible_ = false, .is_bike_accessible_ = false, .is_sidewalk_separate_ = true,}));
+  EXPECT_EQ(91,               cost({.is_foot_accessible_ = false, .is_bike_accessible_ = true,  .is_sidewalk_separate_ = false,}));
+  EXPECT_EQ(osr::kInfeasible, cost({.is_foot_accessible_ = false, .is_bike_accessible_ = true,  .is_sidewalk_separate_ = true,}));
+  EXPECT_EQ(1,                cost({.is_foot_accessible_ = true,  .is_bike_accessible_ = false, .is_sidewalk_separate_ = false,}));
+  EXPECT_EQ(91,               cost({.is_foot_accessible_ = true,  .is_bike_accessible_ = false, .is_sidewalk_separate_ = true,}));
+  EXPECT_EQ(1,                cost({.is_foot_accessible_ = true,  .is_bike_accessible_ = true,  .is_sidewalk_separate_ = false,}));
+  EXPECT_EQ(91,               cost({.is_foot_accessible_ = true,  .is_bike_accessible_ = true,  .is_sidewalk_separate_ = true,}));
+}
