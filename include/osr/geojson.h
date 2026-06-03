e274f0416aa85b3217067dc1b08aeab5ee13c937
Pablo Hoch <pablo@melaxis.com>
2026-06-03 13:31:41 +0200

    conditional parser tests + geojson output

--- include/osr/geojson.h
+++ include/osr/geojson.h
@@ -1,3 +1,9 @@
+#include <algorithm>
+#include <array>
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "boost/json.hpp"
 
 #include "fmt/ranges.h"
@@ -86,6 +92,388 @@ inline char const* to_string(access_value const value) {
     case access_value::kForbidden: return "forbidden";
   }
   return "unknown";
+}
+
+inline char const* to_string(conditional_restriction_field const value) {
+  switch (value) {
+    case conditional_restriction_field::kAccess: return "access";
+    case conditional_restriction_field::kOneway: return "oneway";
+    case conditional_restriction_field::kMaxSpeed: return "maxspeed";
+    case conditional_restriction_field::kMaxLength: return "maxlength";
+    case conditional_restriction_field::kMaxWeightRating:
+      return "maxweightrating";
+    case conditional_restriction_field::kMaxHeight: return "maxheight";
+    case conditional_restriction_field::kMaxWidth: return "maxwidth";
+    case conditional_restriction_field::kMaxWeight: return "maxweight";
+    case conditional_restriction_field::kMaxAxleLoad: return "maxaxleload";
+    case conditional_restriction_field::kMaxAxles: return "maxaxles";
+    case conditional_restriction_field::kHazmat: return "hazmat";
+    case conditional_restriction_field::kHazmatWater: return "hazmat:water";
+    case conditional_restriction_field::kTrailer: return "trailer";
+  }
+  return "other";
+}
+
+inline char const* to_string(conditional_transport_mode const value) {
+  switch (value) {
+    case conditional_transport_mode::kUnspecified: return "";
+    case conditional_transport_mode::kAccess: return "access";
+    case conditional_transport_mode::kVehicle: return "vehicle";
+    case conditional_transport_mode::kMotorVehicle: return "motor_vehicle";
+    case conditional_transport_mode::kMotorcar: return "motorcar";
+    case conditional_transport_mode::kHgv: return "hgv";
+    case conditional_transport_mode::kBus: return "bus";
+    case conditional_transport_mode::kPsv: return "psv";
+    case conditional_transport_mode::kFoot: return "foot";
+    case conditional_transport_mode::kBicycle: return "bicycle";
+  }
+  return "other";
+}
+
+inline char const* to_string(conditional_osm_direction const value) {
+  switch (value) {
+    case conditional_osm_direction::kNone: return "";
+    case conditional_osm_direction::kForward: return "forward";
+    case conditional_osm_direction::kBackward: return "backward";
+  }
+  return "";
+}
+
+inline char const* to_string(conditional_access_value const value) {
+  switch (value) {
+    case conditional_access_value::kUnknown: return "unknown";
+    case conditional_access_value::kYes: return "yes";
+    case conditional_access_value::kDesignated: return "designated";
+    case conditional_access_value::kPermissive: return "permissive";
+    case conditional_access_value::kPrivate: return "private";
+    case conditional_access_value::kNo: return "no";
+    case conditional_access_value::kDestination: return "destination";
+    case conditional_access_value::kDelivery: return "delivery";
+    case conditional_access_value::kDiscouraged: return "discouraged";
+  }
+  return "unknown";
+}
+
+inline char const* to_string(conditional_oneway_value const value) {
+  switch (value) {
+    case conditional_oneway_value::kNo: return "no";
+    case conditional_oneway_value::kForward: return "yes";
+    case conditional_oneway_value::kBackward: return "-1";
+  }
+  return "no";
+}
+
+inline char const* to_string(conditional_comparison const value) {
+  switch (value) {
+    case conditional_comparison::kNone: return "";
+    case conditional_comparison::kEqual: return "=";
+    case conditional_comparison::kLess: return "<";
+    case conditional_comparison::kLessEqual: return "<=";
+    case conditional_comparison::kGreater: return ">";
+    case conditional_comparison::kGreaterEqual: return ">=";
+  }
+  return "";
+}
+
+inline char const* to_string(conditional_vehicle_property const value) {
+  switch (value) {
+    case conditional_vehicle_property::kWeight: return "weight";
+    case conditional_vehicle_property::kWeightRating: return "weight_rating";
+    case conditional_vehicle_property::kLength: return "length";
+    case conditional_vehicle_property::kWidth: return "width";
+    case conditional_vehicle_property::kHeight: return "height";
+    case conditional_vehicle_property::kAxleLoad: return "axleload";
+    case conditional_vehicle_property::kAxles: return "axles";
+  }
+  return "other";
+}
+
+inline char const* to_string(conditional_symbolic_condition const value) {
+  switch (value) {
+    case conditional_symbolic_condition::kDestination: return "destination";
+    case conditional_symbolic_condition::kDelivery: return "delivery";
+    case conditional_symbolic_condition::kPrivate: return "private";
+    case conditional_symbolic_condition::kHazmat: return "hazmat";
+    case conditional_symbolic_condition::kHazmatWater: return "hazmat:water";
+    case conditional_symbolic_condition::kTrailer: return "trailer";
+  }
+  return "other";
+}
+
+inline std::string to_string(conditional_numeric_value const& value) {
+  switch (value.state_) {
+    case conditional_numeric_state::kNone: return "none";
+    case conditional_numeric_state::kValue: break;
+  }
+
+  switch (value.unit_) {
+    case conditional_numeric_unit::kUnitless:
+      return fmt::format("{}", value.value_);
+    case conditional_numeric_unit::kCentimeter:
+      return fmt::format("{} m",
+                         value.value_ / static_cast<double>(100U));
+    case conditional_numeric_unit::kKilometerPerHour:
+      return fmt::format("{} km/h", value.value_);
+    case conditional_numeric_unit::kWeight100Kg:
+      return fmt::format("{} t",
+                         value.value_ / static_cast<double>(10U));
+  }
+  return fmt::format("{}", value.value_);
+}
+
+inline std::string string_value(ways const& w, string_idx_t const idx) {
+  return idx == string_idx_t::invalid() ? ""
+                                        : std::string{w.strings_[idx].view()};
+}
+
+inline char const* month_name(std::uint8_t const month) {
+  constexpr auto const kMonths =
+      std::array{"",    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                 "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+  return month < std::size(kMonths) ? kMonths[month] : "";
+}
+
+inline char const* weekday_name(std::uint8_t const weekday) {
+  constexpr auto const kWeekdays =
+      std::array{"", "Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"};
+  return weekday < std::size(kWeekdays) ? kWeekdays[weekday] : "";
+}
+
+inline std::string to_string(opening_hours_time_span const& span) {
+  auto const time = [](std::uint16_t const minutes) {
+    return fmt::format("{:02}:{:02}", minutes / 60U, minutes % 60U);
+  };
+  return fmt::format("{}-{}", time(span.from_minutes_), time(span.to_minutes_));
+}
+
+inline std::string to_string(opening_hours_weekday_range const& range) {
+  auto const nth = [](std::int8_t const value) {
+    return value == 0 ? std::string{} : fmt::format("[{}]", value);
+  };
+  auto const from =
+      fmt::format("{}{}", weekday_name(range.from_), nth(range.nth_from_));
+  auto const to =
+      fmt::format("{}{}", weekday_name(range.to_), nth(range.nth_to_));
+  return from == to ? from : fmt::format("{}-{}", from, to);
+}
+
+inline std::string to_string(opening_hours_monthday_range const& range) {
+  auto const date = [](opening_hours_date const& d) {
+    if (d.year_ != 0U && d.month_ != 0U && d.day_ != 0U) {
+      return fmt::format("{:04}-{:02}-{:02}", d.year_, d.month_, d.day_);
+    }
+    if (d.month_ != 0U && d.day_ != 0U) {
+      return fmt::format("{} {:02}", month_name(d.month_), d.day_);
+    }
+    if (d.month_ != 0U) {
+      return std::string{month_name(d.month_)};
+    }
+    if (d.year_ != 0U) {
+      return fmt::format("{}", d.year_);
+    }
+    return std::string{"*"};
+  };
+  auto const from = date(range.from_);
+  auto const to = date(range.to_);
+  return from == to ? from : fmt::format("{}-{}", from, to);
+}
+
+template <typename T, typename Fn>
+void append_range(std::stringstream& ss,
+                  char const* prefix,
+                  conditional_range const range,
+                  T const& values,
+                  Fn&& format) {
+  if (range.empty()) {
+    return;
+  }
+  if (ss.tellp() != std::stringstream::pos_type{0}) {
+    ss << " ";
+  }
+  ss << prefix;
+  for (auto i = range.begin_; i != range.end_; ++i) {
+    if (i != range.begin_) {
+      ss << ",";
+    }
+    ss << format(values[i]);
+  }
+}
+
+inline std::string opening_hours_to_string(ways::routing const& r,
+                                           std::uint32_t const idx) {
+  auto ss = std::stringstream{};
+  auto const& oh = r.opening_hours_[idx];
+  for (auto i = oh.rules_.begin_; i != oh.rules_.end_; ++i) {
+    if (i != oh.rules_.begin_) {
+      ss << "; ";
+    }
+    auto rule_ss = std::stringstream{};
+    auto const& rule = r.opening_hours_rules_[i];
+    append_range(
+        rule_ss, "", rule.years_, r.opening_hours_year_ranges_,
+        [](opening_hours_year_range const& range) {
+          auto s = range.from_ == range.to_
+                       ? fmt::format("{}", range.from_)
+                       : fmt::format("{}-{}", range.from_, range.to_);
+          return range.step_ == 1U ? s : fmt::format("{}/{}", s, range.step_);
+        });
+    append_range(
+        rule_ss, "week ", rule.weeks_, r.opening_hours_week_ranges_,
+        [](opening_hours_week_range const& range) {
+          auto s = range.from_ == range.to_
+                       ? fmt::format("{}", range.from_)
+                       : fmt::format("{}-{}", range.from_, range.to_);
+          return range.step_ == 1U ? s : fmt::format("{}/{}", s, range.step_);
+        });
+    append_range(rule_ss, "", rule.monthdays_, r.opening_hours_monthday_ranges_,
+                 [](opening_hours_monthday_range const& range) {
+                   return to_string(range);
+                 });
+    append_range(rule_ss, "", rule.weekdays_, r.opening_hours_weekday_ranges_,
+                 [](opening_hours_weekday_range const& range) {
+                   return to_string(range);
+                 });
+    append_range(
+        rule_ss, "", rule.times_, r.opening_hours_time_spans_,
+        [](opening_hours_time_span const& span) { return to_string(span); });
+    if (rule.modifier_ == opening_hours_rule_modifier::kClosed) {
+      rule_ss << " closed";
+    } else if (rule.modifier_ == opening_hours_rule_modifier::kUnknown) {
+      rule_ss << " unknown";
+    }
+    ss << rule_ss.str();
+  }
+  return ss.str();
+}
+
+inline std::string condition_to_string(ways const& w,
+                                       conditional_condition const& c) {
+  switch (c.type_) {
+    case conditional_condition_type::kOpeningHours:
+      return opening_hours_to_string(*w.r_, c.selector_);
+    case conditional_condition_type::kVehicleProperty:
+      return fmt::format(
+          "{} {} {}",
+          to_string(static_cast<conditional_vehicle_property>(c.selector_)),
+          to_string(c.comparison_), to_string(c.value_));
+    case conditional_condition_type::kAccessPurpose: [[fallthrough]];
+    case conditional_condition_type::kVehicleUsage:
+      return to_string(
+          static_cast<conditional_symbolic_condition>(c.selector_));
+  }
+  return {};
+}
+
+inline std::string condition_set_to_string(
+    ways const& w, conditional_condition_set_idx_t const idx) {
+  auto const& set = w.r_->conditional_condition_sets_[to_idx(idx)];
+  auto ss = std::stringstream{};
+  for (auto i = set.conditions_.begin_; i != set.conditions_.end_; ++i) {
+    if (i != set.conditions_.begin_) {
+      ss << " AND ";
+    }
+    ss << condition_to_string(w, w.r_->conditional_conditions_[i]);
+  }
+  return ss.str();
+}
+
+inline std::string restriction_mode_string(restriction const& r) {
+  auto s = std::string{};
+  s.reserve(3U);
+  s.push_back(r.applies_to_default_ ? 'D' : '-');
+  s.push_back(r.applies_to_bus_ ? 'B' : '-');
+  s.push_back(r.applies_to_hgv_ ? 'H' : '-');
+  return s;
+}
+
+inline std::string restriction_to_string(ways const& w,
+                                         node_idx_t const node,
+                                         restriction const& r) {
+  auto const from_way = w.way_osm_idx_[w.r_->node_ways_[node][r.from_]];
+  auto const to_way = w.way_osm_idx_[w.r_->node_ways_[node][r.to_]];
+  auto out = fmt::format("({}, {}, {})", to_idx(from_way), to_idx(to_way),
+                         restriction_mode_string(r));
+  if (r.condition_set_ != conditional_condition_set_idx_t::invalid()) {
+    out.append(
+        fmt::format(" @ ({})", condition_set_to_string(w, r.condition_set_)));
+  }
+  return out;
+}
+
+template <typename Restriction>
+std::string conditional_key(Restriction const& r) {
+  auto parts = std::vector<std::string>{};
+  if (r.field_ == conditional_restriction_field::kAccess) {
+    if (r.mode_ == conditional_transport_mode::kUnspecified ||
+        r.mode_ == conditional_transport_mode::kAccess) {
+      parts.emplace_back("access");
+    } else {
+      parts.emplace_back(to_string(r.mode_));
+    }
+  } else if (r.field_ == conditional_restriction_field::kHazmat) {
+    parts.emplace_back("hazmat");
+  } else if (r.field_ == conditional_restriction_field::kHazmatWater) {
+    parts.emplace_back("hazmat");
+    parts.emplace_back("water");
+  } else if (r.field_ == conditional_restriction_field::kTrailer) {
+    parts.emplace_back("hgv");
+    parts.emplace_back("trailer");
+  } else {
+    parts.emplace_back(to_string(r.field_));
+    if (r.mode_ == conditional_transport_mode::kHgv) {
+      parts.emplace_back("hgv");
+    }
+  }
+  if (r.direction_ != conditional_osm_direction::kNone) {
+    parts.emplace_back(to_string(r.direction_));
+  }
+  parts.emplace_back("conditional");
+  return fmt::format("{}", fmt::join(parts, ":"));
+}
+
+template <typename Restriction>
+std::string conditional_to_string(ways const& w,
+                                  Restriction const& r,
+                                  std::string const& value) {
+  return fmt::format("{} = {} @ ({})", conditional_key(r), value,
+                     condition_set_to_string(w, r.condition_set_));
+}
+
+inline void add_conditional_properties(boost::json::object& properties,
+                                       ways const& w,
+                                       way_idx_t const way,
+                                       way_properties const& way_props) {
+  properties["has_conditionals"] = way_props.has_conditionals();
+  if (!way_props.has_conditionals()) {
+    return;
+  }
+
+  auto const* conditionals = w.r_->get_conditional_restrictions(way);
+  if (conditionals == nullptr) {
+    return;
+  }
+
+  auto clauses = boost::json::array{};
+  auto append = [&](auto const range, auto const& values,
+                    auto&& value_to_string) {
+    for (auto i = range.begin_; i != range.end_; ++i) {
+      auto const& r = values[i];
+      clauses.emplace_back(conditional_to_string(w, r, value_to_string(r)));
+    }
+  };
+  append(conditionals->access_, w.r_->conditional_access_,
+         [](conditional_access_restriction const& r) {
+           return std::string{to_string(r.value_)};
+         });
+  append(conditionals->oneway_, w.r_->conditional_oneway_,
+         [](conditional_oneway_restriction const& r) {
+           return std::string{to_string(r.value_)};
+         });
+  append(conditionals->numeric_, w.r_->conditional_numeric_,
+         [](conditional_numeric_restriction const& r) {
+           return to_string(r.value_);
+         });
+  properties["conditionals"] = std::move(clauses);
 }
 
 inline void add_hgv_way_info_properties(boost::json::object& properties,
@@ -206,6 +594,7 @@ struct geojson_writer {
           {"is_ramp", p.is_ramp()},
           {"in_route", p.in_route()}};
       add_hgv_way_info_properties(properties, *w_.r_, i, p);
+      add_conditional_properties(properties, w_, i, p);
       features_.emplace_back(boost::json::value{
           {"type", "Feature"},
           {"properties", std::move(properties)},
@@ -242,6 +631,7 @@ struct geojson_writer {
         {"is_ramp", p.is_ramp()},
         {"in_route", p.in_route()}};
     add_hgv_way_info_properties(properties, *w_.r_, i, p);
+    add_conditional_properties(properties, w_, i, p);
 
     features_.emplace_back(
         boost::json::value{{"type", "Feature"},
@@ -290,13 +680,13 @@ struct geojson_writer {
                                            return w_.way_osm_idx_[w];
                                          }))},
           {"restrictions",
-           fmt::format("{}",
-                       w_.r_->node_restrictions_[n] |
-                           std::views::transform([&](restriction const r) {
-                             return std::pair{
-                                 w_.way_osm_idx_[w_.r_->node_ways_[n][r.from_]],
-                                 w_.way_osm_idx_[w_.r_->node_ways_[n][r.to_]]};
-                           }))},
+           fmt::format(
+               "[{}]",
+               fmt::join(w_.r_->node_restrictions_[n] |
+                             std::views::transform([&](restriction const r) {
+                               return restriction_to_string(w_, n, r);
+                             }),
+                         ", "))},
           {"label", ss.str().empty() ? "unreachable" : ss.str()}};
       features_.emplace_back(boost::json::value{
           {"type", "Feature"},
---
+++ test/conditional_extract_test.cc
@@ -0,0 +1,143 @@
+#include <filesystem>
+#include <fstream>
+#include <string_view>
+
+#include "cista/mmap.h"
+
+#include "gtest/gtest.h"
+
+#include "osr/extract/extract.h"
+#include "osr/geojson.h"
+#include "osr/ways.h"
+
+namespace fs = std::filesystem;
+
+namespace {
+
+constexpr auto const kConditionalRestrictionsOsm =
+    R"osm(<osm version="0.6" generator="test">
+  <node id="1" lat="48.0000" lon="9.0000"/>
+  <node id="2" lat="48.0000" lon="9.0010"/>
+  <node id="3" lat="48.0000" lon="9.0020"/>
+  <way id="10">
+    <nd ref="1"/>
+    <nd ref="2"/>
+    <nd ref="3"/>
+    <tag k="highway" v="primary"/>
+    <tag k="access:conditional" v="no @ (Mo-Fr 06:00-10:00)"/>
+    <tag k="maxheight:hgv:conditional" v="3.8 @ (weight &gt; 7.5)"/>
+    <tag k="hazmat:water:conditional" v="no @ (trailer)"/>
+    <tag k="vehicle:conditional" v="no @ (Mo-Fr 06:00-10:00)"/>
+    <tag k="maxstay:conditional" v="30 @ (Mo-Fr 06:00-10:00)"/>
+    <tag k="opening_hours:conditional" v="closed @ (Mo-Fr 06:00-10:00)"/>
+  </way>
+  <way id="20">
+    <nd ref="1"/>
+    <nd ref="2"/>
+    <tag k="highway" v="primary"/>
+    <tag k="access:conditional" v="no @ (PH)"/>
+  </way>
+</osm>)osm";
+
+fs::path write_osm(std::string_view const name,
+                   std::string_view const content) {
+  auto const path = fs::temp_directory_path() / name;
+  auto out = std::ofstream{path};
+  out << content;
+  out.close();
+  return path;
+}
+
+fs::path prepare_extract_dir(std::string_view const name) {
+  auto const dir = fs::temp_directory_path() / name;
+  auto ec = std::error_code{};
+  fs::remove_all(dir, ec);
+  fs::create_directories(dir, ec);
+  return dir;
+}
+
+}  // namespace
+
+TEST(conditional_extract, stores_parseable_conditional_restrictions) {
+  auto const osm_path =
+      write_osm("osr_conditionals.osm", kConditionalRestrictionsOsm);
+  auto const dir = prepare_extract_dir("osr_conditionals_dir");
+
+  osr::extract(false, osm_path.generic_string(), dir, {});
+
+  auto w = osr::ways{dir, cista::mmap::protection::READ};
+  auto const way = w.find_way(osr::osm_way_idx_t{10});
+  ASSERT_TRUE(way.has_value());
+
+  auto const props = w.r_->way_properties_[*way];
+  ASSERT_TRUE(props.has_conditionals());
+  auto const* conditionals = w.r_->get_conditional_restrictions(*way);
+  ASSERT_NE(nullptr, conditionals);
+
+  ASSERT_EQ(2U, conditionals->access_.size());
+  ASSERT_EQ(1U, conditionals->numeric_.size());
+  auto const& access = w.r_->conditional_access_[conditionals->access_.begin_];
+  EXPECT_EQ(osr::conditional_access_value::kNo, access.value_);
+  EXPECT_EQ(osr::conditional_restriction_field::kAccess, access.field_);
+  auto const& hazmat =
+      w.r_->conditional_access_[conditionals->access_.begin_ + 1U];
+  EXPECT_EQ(osr::conditional_restriction_field::kHazmatWater, hazmat.field_);
+  EXPECT_EQ(osr::conditional_access_value::kNo, hazmat.value_);
+
+  auto const& maxheight =
+      w.r_->conditional_numeric_[conditionals->numeric_.begin_];
+  EXPECT_EQ(osr::conditional_restriction_field::kMaxHeight, maxheight.field_);
+  EXPECT_EQ(osr::conditional_transport_mode::kHgv, maxheight.mode_);
+  EXPECT_EQ(osr::conditional_numeric_unit::kCentimeter, maxheight.value_.unit_);
+  EXPECT_EQ(380U, maxheight.value_.value_);
+
+  ASSERT_EQ(3U, w.r_->conditional_condition_sets_.size());
+  ASSERT_EQ(1U, w.r_->opening_hours_weekday_ranges_.size());
+  EXPECT_EQ(1U, w.r_->opening_hours_weekday_ranges_[0].from_);
+  EXPECT_EQ(5U, w.r_->opening_hours_weekday_ranges_[0].to_);
+  ASSERT_EQ(1U, w.r_->opening_hours_time_spans_.size());
+  EXPECT_EQ(6U * 60U, w.r_->opening_hours_time_spans_[0].from_minutes_);
+  EXPECT_EQ(10U * 60U, w.r_->opening_hours_time_spans_[0].to_minutes_);
+}
+
+TEST(conditional_extract, writes_conditionals_to_geojson_debug_output) {
+  auto const osm_path =
+      write_osm("osr_conditionals_geojson.osm", kConditionalRestrictionsOsm);
+  auto const dir = prepare_extract_dir("osr_conditionals_geojson_dir");
+
+  osr::extract(false, osm_path.generic_string(), dir, {});
+
+  auto w = osr::ways{dir, cista::mmap::protection::READ};
+  auto const way = w.find_way(osr::osm_way_idx_t{10});
+  ASSERT_TRUE(way.has_value());
+
+  auto writer = osr::geojson_writer{w};
+  writer.write_way(*way);
+  auto const json = writer.string();
+
+  EXPECT_NE(std::string::npos, json.find("\"has_conditionals\":true"));
+  EXPECT_NE(std::string::npos,
+            json.find("access:conditional = no @ (Mo-Fr 06:00-10:00)"));
+  EXPECT_NE(std::string::npos,
+            json.find("maxheight:hgv:conditional = 3.8 m @ (weight > 7.5 t)"));
+  EXPECT_NE(std::string::npos,
+            json.find("hazmat:water:conditional = no @ (trailer)"));
+  EXPECT_EQ(std::string::npos, json.find("vehicle:conditional"));
+  EXPECT_EQ(std::string::npos, json.find("maxstay:conditional"));
+  EXPECT_EQ(std::string::npos, json.find("opening_hours:conditional"));
+}
+
+TEST(conditional_extract, ignores_unsupported_conditionals) {
+  auto const osm_path = write_osm("osr_conditionals_unsupported.osm",
+                                  kConditionalRestrictionsOsm);
+  auto const dir = prepare_extract_dir("osr_conditionals_unsupported_dir");
+
+  osr::extract(false, osm_path.generic_string(), dir, {});
+
+  auto w = osr::ways{dir, cista::mmap::protection::READ};
+  auto const way = w.find_way(osr::osm_way_idx_t{20});
+  ASSERT_TRUE(way.has_value());
+
+  EXPECT_FALSE(w.r_->way_properties_[*way].has_conditionals());
+  EXPECT_EQ(nullptr, w.r_->get_conditional_restrictions(*way));
+}
