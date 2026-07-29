23ef38827decf9a0ffe5ed170dd52cf0b312b759
Jurek Elliesen <91612967+aff3npirat@users.noreply.github.com>
2026-07-29 14:15:53 +0200

    wip

--- include/osr/routing/dijkstra.h
+++ include/osr/routing/dijkstra.h
@@ -83,7 +83,7 @@ struct dijkstra {
            bitvec<node_idx_t> const* blocked,
            sharing_data const* sharing,
            elevation_storage const* elevations) {
-    while (!pq_.empty()) {
+    while (!pq_.empty() && pq_.current_bucket_ < max) {
       auto l = pq_.pop();
 
       if (get_cost(l.get_node()) < l.cost()) {
@@ -134,6 +134,7 @@ struct dijkstra {
             auto const total = static_cast<std::uint64_t>(l.cost()) + cost;
             if (total >= max) {
               max_reached_ = true;
+              pq_.push({l, total});
               return;
             }
             if (cost_[neighbor.get_key()].update(
