152a59a8edd03207eb2ccfbfbd2eb3b8ab2bfaa7
Michael Kutzner <174690291+MichaelKutzner@users.noreply.github.com>
2026-08-03 07:46:33 +0200

    Remove some debug code

--- src/routing/for_each_parking_edge.cc
+++ src/routing/for_each_parking_edge.cc
@@ -7,7 +7,6 @@ namespace osr {
 void for_each_parking_edge(ways::routing const& r,
                            node_idx_t const node_idx,
                            std::function<void(parking_edge_idx_t)> const& f) {
-	fmt::println("∀parking_edge( {} )", node_idx);
   if (r.has_parking_edges_.test(node_idx)) {
     auto it = std::lower_bound(
         begin(r.node_parking_edges_), end(r.node_parking_edges_), node_idx,
@@ -15,10 +14,8 @@ void for_each_parking_edge(ways::routing const& r,
           return p.first < n;
         });
     for (; it != end(r.node_parking_edges_) && it->first == node_idx; ++it) {
-			fmt::println("{}, ", it->second);
       f(it->second);
     }
-		fmt::println("  DONE");
   }
 }
 
